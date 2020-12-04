55e0f0424880ff94234e4557fed58b1bf4557548
suffocating111 <68234327+suffocating111@users.noreply.github.com>
2020-12-04 22:13:53 +0100

    @____@

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const logFileText = async file => {
         for (let i = 0; i < zul.split("\n").length; ++i) {
             firstLetter = zul.split("\n")[i].charAt(0).toUpperCase();
             noFirst = zul.split("\n")[i].substring(1);
-            result.push(firstLetter + noFirst);
+            result.push((firstLetter + noFirst).trim()+" ");
         }
 
         $.each(result, function (i, el) {
@@ -13,11 +13,11 @@ const logFileText = async file => {
         });
 
         let xd = uniqueNames.sort((a, b) => a.localeCompare(b))
-
+        let skript = document.getElementById('script').innerHTML;
         for(let i = 0; i<xd.length; ++i) {
-            document.getElementById('script').innerHTML += xd[i];
+            skript += xd[i];
         }
-        document.getElementById('script').innerHTML = document.getElementById('script').innerHTML.substring(0, document.getElementById('script').innerHTML.length - 2);
+        document.getElementById('script').innerHTML = skript.substring(0, skript.length - 2);
         document.getElementById('counter').innerHTML = "Names in total: " + xd.length;
     });
 }
